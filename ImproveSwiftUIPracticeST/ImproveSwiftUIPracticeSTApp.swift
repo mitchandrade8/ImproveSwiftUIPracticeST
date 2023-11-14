c782175da85a1de133d85fd55437eb8e233d63ad
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-13 21:43:46 -0700

    Initial Commit

---
+++ ImproveSwiftUIPracticeST/ImproveSwiftUIPracticeSTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImproveSwiftUIPracticeSTApp.swift
+//  ImproveSwiftUIPracticeST
+//
+//  Created by Mitch Andrade on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ImproveSwiftUIPracticeSTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
