d96368719f468dfa0f78d4ca040125185f538a85
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-13 22:01:24 -0700

    --Setting up LongPressGesture

---
+++ ImproveSwiftUIPracticeST/LongPressGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  LongPressGestureBootcamp.swift
+//  ImproveSwiftUIPracticeST
+//
+//  Created by Mitch Andrade on 11/13/23.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongPressGestureBootcamp()
+}
