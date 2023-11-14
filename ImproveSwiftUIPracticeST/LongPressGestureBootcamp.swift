c2a98e7ab0cd2875f3cc17cd7102ac3f2bdb5272
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-13 22:20:32 -0700

    --Progress: Building a real life example where we would use a longTapGesture

--- ImproveSwiftUIPracticeST/LongPressGestureBootcamp.swift
+++ ImproveSwiftUIPracticeST/LongPressGestureBootcamp.swift
@@ -12,14 +12,30 @@ struct LongPressGestureBootcamp: View {
     @State var isComplete: Bool = false
     
     var body: some View {
-        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
-            .padding()
-            .padding(.horizontal)
-            .background(isComplete ? Color.green : Color.gray)
-            .cornerRadius(10)
-            .onLongPressGesture {
-                isComplete.toggle()
+        
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(maxWidth: 100)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundStyle(.white)
             }
+            
+        }
+        
+//        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+//            .onLongPressGesture(minimumDuration: 5.0, maximumDistance: 1) {
+//                isComplete.toggle()
+//            }
 //            .onTapGesture {
 //                isComplete.toggle()
 //            }
